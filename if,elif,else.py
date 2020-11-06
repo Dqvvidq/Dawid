5291017af12a1bdc726c9c8c910293f386c26415
Dqvvidq <74021650+Dqvvidq@users.noreply.github.com>
2020-11-06 14:46:57 +0100

    Add files via upload

---
+++ if,elif,else.py
@@ -0,0 +1,59 @@
+home = "Ameryka"
+if home == "Ameryka":
+    print("Witaj Ameryko")
+else:
+    print("Witaj Swiecie")
+
+
+home = "Polska"
+if home == "Polska":
+    print("Witaj Polsko")
+
+x = 3
+if x == 2:
+    print("x jest rowne 2")
+if x % 2 == 0:
+    print("liczba jest parzysta")
+if x % 2 != 0:
+    print("liczba nie jest parzysta")
+
+z = 10
+y = 11
+
+if z == 10:
+    if y == 11:
+        print(z+y)
+
+home = "Polska"
+if home == "Tajlandia":
+    print("Witaj Tajlandio")
+elif home == "Polska":
+    print("Witaj Polsko")
+elif home == "Holandia":
+    print ("Witaj Holandio")
+else:
+    print("Nie ma takiego kraju")
+
+imie = "Dawid"
+if imie == "Adam":
+    print("Witaj Adamie")
+else:
+    print("Nie ma takiego imienia")
+
+x = 100
+if x == 20:
+    print("X wynosi 20")
+elif x == 30:
+    print("X wynosi 30")
+else:
+    print("Nie wiem")
+
+if x == 100:
+    print("X jest równe 100!!")
+
+if x % 2 == 0:
+    print("X jest parzyste")
+else:
+    print("X nie jest parzyste")
+
+
