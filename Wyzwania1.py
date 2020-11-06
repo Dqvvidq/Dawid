dc634d831c4df9a399d521b374ac75af3b01cdc1
Dqvvidq <74021650+Dqvvidq@users.noreply.github.com>
2020-11-06 17:35:31 +0100

    Add files via upload

---
+++ Wyzwania1.py
@@ -0,0 +1,29 @@
+#zadanie 1
+print("Siema")
+print("Cześć")
+print("Witaj")
+
+#zadanie 2
+x = 11
+if x < 10:
+    print("Wartość zmiennej jest mniejsze od 10")
+else:
+    print("Wartość zmiennej jest większe lub równe 10")
+
+#zadanie 3
+
+z = 25
+if z <= 10:
+    print("x jest mniejsza lub równa 10")
+elif z > 10:
+    if z <= 25:
+        print("wartość zmiennej jest większa od 10 i mniejsza lub równa 25")
+if z == 25:
+    print("z jest rowne 25")
+
+a = 6
+b = 4
+
+print(a//b)
+print(a%b)
+
