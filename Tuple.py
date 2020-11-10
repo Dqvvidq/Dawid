065bd0a4dcedef00c7717c89188410e3c1f0fb5e
Dqvvidq <74021650+Dqvvidq@users.noreply.github.com>
2020-11-10 14:42:29 +0100

    Add files via upload

---
+++ Tuple.py
@@ -0,0 +1,28 @@
+# tworzenie krotki
+#krotki nie są modyfikowalne
+
+moj_tupel = ()
+print(moj_tupel)
+
+
+rnd = ("m", 101, True, 101.2, "Witaj")
+print(rnd)
+
+# do krotki nie można dodawać nowych elementów ani ich zmieniac
+
+#rnd[1] = 200
+
+# elementy krotki mozna pobierac
+
+print(rnd[3])
+
+# można sprawdzić czy dany element znajduje sie w liscie lub nie
+
+print(101 in rnd)
+
+print(False not in rnd)
+
+
+
+
+
