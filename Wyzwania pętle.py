b3cc1fe8fea213f90e1b4e9ca03328e413bfd957
Dqvvidq <74021650+Dqvvidq@users.noreply.github.com>
2020-11-21 19:00:20 +0100

    Add files via upload

---
+++ Wyzwania pętle.py
@@ -0,0 +1,37 @@
+# wyswietl kazdy element z listy
+
+lista = ["noc zywych trupow", "Ekipa", "Rodzina soprano", "Pamietnik wampirow"]
+
+for filmy in lista:
+    print(filmy)
+
+# Wyświetl wszystkie liczby z zakresu od 25 do 50
+
+for p in range(25, 51):
+    print(p)
+
+# Wyswielt wszystkie elementy listy z wyzwania pierszego z indeksami
+
+for c, list in enumerate(lista):
+    print(c)
+    print(list)
+
+
+#pomnoz wszystkie liczby z zakresu
+
+z1 = [8, 19, 148, 4]
+z2 = [9, 1, 33, 83]
+
+for i in z1:
+    for j in z2:
+        print(i * j)
+
+
+
+
+
+
+
+
+
+
